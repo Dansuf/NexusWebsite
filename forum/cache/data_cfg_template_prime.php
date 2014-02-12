5533dbcfe13e44df8efadd4e449a5a1d1f70c0a8
Dan Sufho <tomasz.ks9@gmail.com>
2014-02-12 21:59:08 +0100

    Deleting/ignoring files - attempt 2

--- forum/cache/data_cfg_template_prime.php
+++
@@ -1,4 +0,0 @@
-<?php exit; ?>
-1415558170
-205
-a:6:{s:4:"name";s:5:"Prime";s:9:"copyright";s:26:"&copy; Abhishek Srivastava";s:7:"version";s:5:"1.1.2";s:17:"template_bitfield";s:4:"lNg=";s:12:"inherit_from";s:9:"prosilver";s:8:"filetime";i:1381472484;}
--- forum/cache/data_cfg_theme_fractal.php
+++
@@ -1,4 +0,0 @@
-<?php exit; ?>
-1419112394
-152
-a:5:{s:4:"name";s:7:"fractal";s:9:"copyright";s:25:"&copy; Tomasz Kuczak 2013";s:7:"version";b:1;s:14:"parse_css_file";b:1;s:8:"filetime";i:1387576109;}
