734ed33fbe5bba12521901ddf638fc73339bf7fb
Dan Sufho <tomasz.ks9@gmail.com>
2014-02-15 20:55:46 +0100

    Changed codename from "Fractal" to "Nexus"

---
+++ modules/mod_nexussidebar/mod_nexussidebar.php
@@ -0,0 +1,155 @@
+<?php
+
+// no direct access
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+global $phpbb_root_path, $phpEx, $user, $db, $config, $cache, $template, $auth;
+
+define('IN_PHPBB', true);
+$phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : 'forum/';
+$phpEx = substr(strrchr(__FILE__, '.'), 1);
+include($phpbb_root_path . 'common.' . $phpEx); 
+include($phpbb_root_path . 'includes/bbcode.' . $phpEx);
+include($phpbb_root_path . 'includes/functions_display.' . $phpEx);
+
+// Start session management
+$user->session_begin();
+$auth->acl($user->data);
+$user->setup('ucp');
+
+$friends_online = [];
+$friends_offline = [];
+$coins;
+$newNotification = false;
+
+$u_images = 'media/mod_nexussidebar/images/';
+
+define('MAX_FRIENDS',5); // Max number of friends in friends list
+
+if ($user->data['user_id'] != ANONYMOUS)
+{	
+	$u_privatemsgs = append_sid("{$phpbb_root_path}ucp.$phpEx", 'i=pm&amp;folder=inbox');
+	$u_friend = append_sid("{$phpbb_root_path}ucp.$phpEx", 'i=zebra&amp;mode=friends');
+	$u_profile = append_sid("{$phpbb_root_path}ucp.$phpEx");
+	$u_search_self = append_sid("{$phpbb_root_path}search.$phpEx", 'search_id=egosearch');
+	$u_logout = JRoute::_('index.php?option=com_users&task=user.logout').'&'.JUtility::getToken().'=1';
+	
+	/* copy from functions.php */
+	$l_privmsgs_text = $l_privmsgs_text_unread = '';
+	
+	// Obtain number of new private messages if user is logged in
+	if (!empty($user->data['is_registered']))
+	{
+		if ($user->data['user_new_privmsg'])
+		{
+			$l_message_new = ($user->data['user_new_privmsg'] == 1) ? $user->lang['NEW_PM'] : $user->lang['NEW_PMS'];
+			$l_privmsgs_text = sprintf($l_message_new, $user->data['user_new_privmsg']);
+	
+			if (!$user->data['user_last_privmsg'] || $user->data['user_last_privmsg'] > $user->data['session_last_visit'])
+			{
+				$sql = 'UPDATE ' . USERS_TABLE . '
+					SET user_last_privmsg = ' . $user->data['session_last_visit'] . '
+					WHERE user_id = ' . $user->data['user_id'];
+				$db->sql_query($sql);
+			}
+			$newNotification = true;
+		}
+		else
+		{
+			$l_privmsgs_text = $user->lang['NO_NEW_PM'];
+		}
+	
+		$l_privmsgs_text_unread = '';
+	
+		if ($user->data['user_unread_privmsg'] && $user->data['user_unread_privmsg'] != $user->data['user_new_privmsg'])
+		{
+			$l_message_unread = ($user->data['user_unread_privmsg'] == 1) ? $user->lang['UNREAD_PM'] : $user->lang['UNREAD_PMS'];
+			$l_privmsgs_text_unread = sprintf($l_message_unread, $user->data['user_unread_privmsg']);
+		}
+	}
+	/* functions.php end */
+	
+	/* Code below is a modified copy from ucp.php */
+
+	// Output listing of friends online
+	$update_time = $config['load_online_time'] * 60;
+
+	$sql = $db->sql_build_query('SELECT_DISTINCT', array(
+			'SELECT'	=> 'u.user_id, u.username, u.username_clean, u.user_colour, MAX(s.session_time) as online_time, MIN(s.session_viewonline) AS viewonline',
+
+			'FROM'		=> array(
+					USERS_TABLE		=> 'u',
+					ZEBRA_TABLE		=> 'z'
+			),
+
+			'LEFT_JOIN'	=> array(
+					array(
+							'FROM'	=> array(SESSIONS_TABLE => 's'),
+							'ON'	=> 's.session_user_id = z.zebra_id'
+					)
+			),
+
+			'WHERE'		=> 'z.user_id = ' . $user->data['user_id'] . '
+		AND z.friend = 1
+		AND u.user_id = z.zebra_id',
+
+			'GROUP_BY'	=> 'z.zebra_id, u.user_id, u.username_clean, u.user_colour, u.username',
+
+			'ORDER_BY'	=> 'u.username_clean ASC',
+	));
+
+	$result = $db->sql_query($sql);
+	
+	while ($row = $db->sql_fetchrow($result))
+	{
+		$which = (time() - $update_time < $row['online_time'] && ($row['viewonline'] || $auth->acl_get('u_viewonline')));
+		
+		if($which)
+		{
+			$friends_online[] = '<li><img class="icon-img" src="' . $u_images . 'online.png "> <span><a href="' . get_username_string('profile', $row['user_id'], $row['username'], $row['user_colour']) . '">' . $row['username'] . '</a>'
+			. "</span></li>";
+		}
+		else
+		{
+			$friends_offline[] = '<li><img class="icon-img" src="' . $u_images . 'offline.png "> <span><a href="' . get_username_string('profile', $row['user_id'], $row['username'], $row['user_colour']) . '">' . $row['username'] . '</a>'
+			. "</span></li>";
+		}
+		
+		/*
+		$template->assign_block_vars("friends_{$which}", array(
+				'USER_ID'		=> $row['user_id'],
+
+				'U_PROFILE'		=> get_username_string('profile', $row['user_id'], $row['username'], $row['user_colour']),
+				'USER_COLOUR'	=> get_username_string('colour', $row['user_id'], $row['username'], $row['user_colour']),
+				'USERNAME'		=> get_username_string('username', $row['user_id'], $row['username'], $row['user_colour']),
+				'USERNAME_FULL'	=> get_username_string('full', $row['user_id'], $row['username'], $row['user_colour']))
+		);
+		*/
+		
+	}
+	$db->sql_freeresult($result);
+
+	/* copy end */
+
+	make_jumpbox(append_sid("{$phpbb_root_path}viewforum.$phpEx"));
+	
+	// Grab info for profile section
+	
+	$sql = "SELECT *
+	FROM " . PROFILE_FIELDS_DATA_TABLE . "
+	WHERE user_id =" . $user->data['user_id'];
+	$result = $db->sql_query($sql);
+	$row = $db->sql_fetchrow($result);
+	$db->sql_freeresult($result);
+	$coins = $row['pf_coins'] ? $row['pf_coins'] : 0;
+	
+	
+}
+
+$warningImage = JURI::base() . 'images/Warning.png';
+$username = $user->data['username'];
+$avatar = get_user_avatar($user->data['user_avatar'], $user->data['user_avatar_type'], $user->data['user_avatar_width'], $user->data['user_avatar_height'], 'USER_AVATAR', true);
+$cookieIssueInfo = $params->get('cookie-issue-info');
+
+require( JModuleHelper::getLayoutPath( 'mod_nexussidebar' ) );
+?>
---
+++ modules/mod_nexussidebar/tmpl/default.php
@@ -0,0 +1,126 @@
+<?php // no direct access
+defined( '_JEXEC' ) or die( 'Restricted access' ); ?>
+
+<div id="usr-nav-reference" onclick="showNav()">
+	<div class="hex" id="usr-nav-reference-hex">
+		<div>
+			<?php if($user->data['user_id'] != ANONYMOUS || $cookieIssueInfo): ?>
+			<?php if($newNotification) echo '<img src="' . $u_images . 'warning_big.png" style="padding-right: 0;">'; echo $avatar; ?><span><?php echo $username ?></span>
+			<?php else: ?>
+			<img src="<?php echo $u_images; ?>warning_big.png"><span><?php echo JText::_('MOD_NSIDEBAR_COOKIE_ISSUE'); ?></span>
+			<?php endif ?>
+		</div>
+	</div>
+</div>
+<div id="usr-nav">
+			<div id="usr-nav-wrap">
+				<div>
+				<?php if($user->data['user_id'] != ANONYMOUS || $cookieIssueInfo): ?>
+				<div id="usr-nav-name">
+							<?php echo $avatar ?><span><?php echo $username ?></span><div class="heex"><div></div></div>
+				</div>
+				<hr class="sep">
+				<div class="usr-nav-section">
+					<h3><img src="<?php echo $u_images . 'warning.png'; ?>"> <span><?php echo JText::_('MOD_NSIDEBAR_NOTIFICATIONS'); ?>:</span></h3>
+					<ul>
+						<li><img class="icon-img" src="<?php echo $u_images . 'envelope.png'; ?>"> <a href="<?php echo $u_privatemsgs; ?>"><?php echo $l_privmsgs_text; ?></a></li>
+					</ul>
+				</div>
+				<hr class="sep">
+				<div class="usr-nav-section">
+					<h3><img src="<?php echo $u_images . 'friends.png'; ?>"> <span><?php echo JText::_('MOD_NSIDEBAR_FRIENDS'); ?>:</span></h3>
+					<ul>
+						<?php 
+						if($friends_online || $friends_offline)
+						{
+							if(sizeof($friends_online) + sizeof($friends_offline) <= MAX_FRIENDS)
+							{
+								foreach($friends_online as &$friend)
+								{
+									echo $friend;
+								}
+								foreach($friends_offline as &$friend)
+								{
+									echo $friend;
+								}
+							} 
+							else if(sizeof($friends_online) > MAX_FRIENDS)
+							{
+								for($i=0;$i < MAX_FRIENDS; $i++)
+								{
+									echo $friends_online[$i];
+								}
+							}
+							else
+							{
+								foreach($friends_online as &$friend)
+								{
+									echo $friend;
+								}
+								if(sizeof($friends_offline) < (MAX_FRIENDS-sizeof($friends_online)))
+								{
+									for($i=0;$i < sizeof(friends_offline); $i++)
+									{
+										echo $friends_offline[$i];
+									}
+								}
+								else 
+								{
+									for($i=0;$i < (MAX_FRIENDS-sizeof($friends_online)); $i++)
+									{
+										echo $friends_offline[$i];
+									}
+								}
+							}
+						}
+						else
+							echo JText::_('MOD_NSIDEBAR_NO_FRIENDS');
+						?>
+						<li class="more"><a href="<?php echo $u_friend; ?>"><?php echo JText::_('MOD_NSIDEBAR_MORE'); ?> <img class="down-img" src="<?php echo $u_images . 'down.png'; ?>"></a></li>
+					</ul>
+				</div>
+				<hr class="sep">
+				<div class="usr-nav-section">
+					<h3><img src="<?php echo $u_images . 'profile.png'; ?>"> <span><?php echo JText::_('MOD_NSIDEBAR_PROFILE'); ?>:</span></h3>
+					<table>
+						<tr><td><img class="icon-img" src="<?php echo $u_images . 'coin.png'; ?>"> <span><?php echo JText::_('MOD_NSIDEBAR_COINS'); ?>:</span></td><td><?php echo $coins; ?></td></tr>
+						<tr><td><img class="icon-img" src="<?php echo $u_images . 'clock.png'; ?>"> <span><?php echo JText::_('MOD_NSIDEBAR_TIME_PLAYED'); ?>:</span></td><td>2h30min</td></tr>
+					</table>
+					<div class="more"><a href="<?php echo append_sid("{$phpbb_root_path}memberlist.$phpEx", 'mode=viewprofile&amp;u=' . $user->data['user_id']); ?>"><?php echo JText::_('MOD_NSIDEBAR_MORE'); ?> <img class="down-img" src="<?php echo $u_images . 'down.png'; ?>"></a></div>
+				</div>
+				<hr class="sep">
+				<div class="usr-nav-section">
+					<h3><img src="<?php echo $u_images . 'stats.png'; ?>"> <span><?php echo JText::_('MOD_NSIDEBAR_STATS'); ?>:</span></h3>
+					<ul>
+						<li>Heroes <img class="down-img" src="<?php echo $u_images . 'down.png'; ?>"></li>
+						<li>Craftplay <img class="down-img" src="<?php echo $u_images . 'down.png'; ?>"></li>
+						<li>Bananacraft <img class="down-img" src="<?php echo $u_images . 'down.png'; ?>"></li>
+						<li class="more"><?php echo JText::_('MOD_NSIDEBAR_MORE'); ?> <img class="down-img" src="<?php echo $u_images . 'down.png'; ?>"></li>
+					</ul>
+				</div>
+				<?php else: ?>
+				<div id="usr-nav-name">
+							<img src="<?php echo $u_images; ?>warning_big.png"><span><?php echo JText::_('MOD_NSIDEBAR_COOKIE_ISSUE'); ?></span><div class="heex"><div></div></div>
+				</div>
+				<hr class="sep">
+				<div class="usr-nav-section">
+				<?php echo JText::_('MOD_NSIDEBAR_COOKIE_ISSUE_DESC'); ?>
+				</div>
+				<?php endif ?>
+				<hr class="sep">
+				<div class="usr-nav-section">
+					<h3><img src="<?php echo $u_images . 'others.png'; ?>"> <span><?php echo JText::_('MOD_NSIDEBAR_OTHER'); ?>:</span></h3>
+					<ul>
+						<li><img class="icon-img" src="<?php echo $u_images . 'ucp.png'; ?>"> <a href="<?php echo $u_profile; ?>"><?php echo JText::_('MOD_NSIDEBAR_UCP'); ?></a></li>
+						<?php if($auth->acl_get('a_') && !empty($user->data['is_registered'])) echo '<li><img class="icon-img" src="' . $u_images . 'adminthing.png"> <a href="' . append_sid("{$phpbb_root_path}adm/index.$phpEx", false, true, $user->session_id) .'">' . JText::_('MOD_NSIDEBAR_ACP') . '</a></li>'; ?>
+						<?php if(JFactory::getUser()->authorise('core.edit', 'com_contact')) echo '<li><img class="icon-img" src="' . $u_images . 'adminthing.png"> <a href="administrator">' . JText::_('MOD_NSIDEBAR_JOOMLA_ADMIN') . '</a>'; ?>
+						<?php if(JFactory::getUser()->authorise('core.create', 'com_content')) echo '<li><img class="icon-img" src="' . $u_images . 'newarticle.png"> <a href="/joomla/index.php/submit-an-article">' . JText::_('MOD_NSIDEBAR_NEW_ARTICLE') . '</a></li>'; ?>
+						<li><img class="icon-img" src="<?php echo $u_images . 'viewposts.png'; ?>"> <a href="<?php echo $u_search_self; ?>"><?php echo JText::_('MOD_NSIDEBAR_POSTS'); ?></a></li>
+						<li><img class="icon-img" src="<?php echo $u_images . 'donate.png'; ?>"> <a href="#"><?php echo JText::_('MOD_NSIDEBAR_DONATE'); ?></a></li>
+						<li><img class="icon-img" src="<?php echo $u_images . 'logout.png'; ?>"> <a href="/joomla/index.php/logout"><?php echo JText::_('MOD_NSIDEBAR_LOGOUT'); ?></a></li>
+					</ul>
+				</div>
+				</div>				
+			</div>
+		</div> 
+		<script type="text/javascript" src="<?php echo 'modules/'.$module->module.'/scripts/main.js' ?>"></script> 
